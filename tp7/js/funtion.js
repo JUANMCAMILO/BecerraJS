c93e7305077b0aaa2d039c0df4acc31910f6a3a9
Kravmaga01 <edwardsalex8034@gmil.com>
2022-06-06 17:41:22 -0500

    tercer commit

---
+++ tp7/js/funtion.js
@@ -0,0 +1,143 @@
+let boton1 =document.getElementById("J-b1");
+let boton2 =document.getElementById("J-b2");
+let boton3 =document.getElementById("J-b3");
+let boton4 =document.getElementById("J-b4");
+let boton5 =document.getElementById("J-b5");
+let boton6 =document.getElementById("J-b6");
+let boton7 =document.getElementById("J-b7");
+let boton8 =document.getElementById("J-b8");
+let boton9 =document.getElementById("J-b9");
+
+let botonJ1 =document.getElementById("C-b1");
+let botonJ2 =document.getElementById("C-b2");
+let botonJ3 =document.getElementById("C-b3");
+let botonJ4 =document.getElementById("C-b4");
+let botonJ5 =document.getElementById("C-b5");
+let botonJ6 =document.getElementById("C-b6");
+let botonJ7 =document.getElementById("C-b7");
+let botonJ8 =document.getElementById("C-b8");
+let botonJ9 =document.getElementById("C-b9");
+
+let caja1 =document.getElementById("J-b1");
+let caja2 =document.getElementById("J-b2");
+let caja3 =document.getElementById("J-b3");
+let caja4 =document.getElementById("J-b4");
+let caja5 =document.getElementById("J-b5");
+let caja6 =document.getElementById("J-b6");
+let caja7 =document.getElementById("J-b7");
+let caja8 =document.getElementById("J-b8");
+let caja9 =document.getElementById("J-b9");
+
+let cajas1 =document.getElementById("C-b1");
+let cajas2 =document.getElementById("C-b2");
+let cajas3 =document.getElementById("C-b3");
+let cajas4 =document.getElementById("C-b4");
+let cajas5 =document.getElementById("C-b5");
+let cajas6 =document.getElementById("C-b6");
+let cajas7 =document.getElementById("C-b7");
+let cajas8 =document.getElementById("C-b8");
+let cajas9 =document.getElementById("C-b9");
+
+ boton1.addEventListener("click",rojo1);
+  caja1.classList.remove("blanco");
+  caja1.classList.remove("verde");
+  caja1.classList.remove("rojo");
+
+  boton2.addEventListener("click",rojo2);
+  caja2.classList.remove("blanco");
+  caja2.classList.remove("verde");
+  caja2.classList.remove("rojo");
+
+  boton3.addEventListener("click",rojo3);
+  caja3.classList.remove("blanco");
+  caja3.classList.remove("verde");
+  caja3.classList.remove("rojo");
+
+  boton4.addEventListener("click",rojo4);
+  caja4.classList.remove("blanco");
+  caja4.classList.remove("verde");
+  caja4.classList.remove("rojo");
+
+  boton5.addEventListener("click",rojo5);
+  caja5.classList.remove("blanco");
+  caja5.classList.remove("verde");
+  caja5.classList.remove("rojo");
+
+  boton6.addEventListener("click",rojo6);
+  caja6.classList.remove("blanco");
+  caja6.classList.remove("verde");
+  caja6.classList.remove("rojo");
+
+  boton7.addEventListener("click",rojo7);
+  caja7.classList.remove("blanco");
+  caja7.classList.remove("verde");
+  caja7.classList.remove("rojo");
+
+  boton8.addEventListener("click",rojo8);
+  caja8.classList.remove("blanco");
+  caja8.classList.remove("verde");
+  caja8.classList.remove("rojo");
+
+  boton9.addEventListener("click",rojo9);
+  caja9.classList.remove("blanco");
+  caja9.classList.remove("verde");
+  caja9.classList.remove("rojo");
+
+
+
+  botonJ1.addEventListener("click",verde1);
+  function verde1(){
+    cajas1.classList.remove("blanco");
+    cajas1.classList.remove("verde");
+    cajas1.classList.remove("rojo");
+  }
+  botonJ2.addEventListener("click",verde2);
+  function verde2(){
+    cajas2.classList.remove("blanco");
+    cajas2.classList.remove("verde");
+    cajas2.classList.remove("rojo");
+  }
+  botonJ3.addEventListener("click",verde3);
+  function verde3(){
+    cajas3.classList.remove("blanco");
+    cajas3.classList.remove("verde");
+    cajas3.classList.remove("rojo");
+  }
+  botonJ4.addEventListener("click",verde4);
+  function verde4(){
+    cajas4.classList.remove("blanco");
+    cajas4.classList.remove("verde");
+    cajas4.classList.remove("rojo");
+  }
+  botonJ5.addEventListener("click",verde5);
+  function verde5(){
+    cajas5.classList.remove("blanco");
+    cajas5.classList.remove("verde");
+    cajas5.classList.remove("rojo");
+  }
+  botonJ6.addEventListener("click",verde6);
+  function verde6(){
+    cajas6.classList.remove("blanco");
+    cajas6.classList.remove("verde");
+    cajas6.classList.remove("rojo");
+  }
+  botonJ7.addEventListener("click",verde7);
+  function verde7(){
+    cajas7.classList.remove("blanco");
+    cajas7.classList.remove("verde");
+    cajas7.classList.remove("rojo");
+  }
+  botonJ8.addEventListener("click",verde8);
+  function verde8(){
+    cajas8.classList.remove("blanco");
+    cajas8.classList.remove("verde");
+    cajas8.classList.remove("rojo");
+  }
+  botonJ9.addEventListener("click",verde9);
+  function verde9(){
+    cajas9.classList.remove("blanco");
+    cajas9.classList.remove("verde");
+    cajas9.classList.remove("rojo");
+  }
+
+  
