d768c88cb66c55ce636b6a563de4bd531aae72ce
Kravmaga01 <edwardsalex8034@gmil.com>
2022-06-09 17:27:58 -0500

    quinto commit

---
+++ tp8/js/Array.js
@@ -0,0 +1,15 @@
+const num = 3;
+//num= 4;
+edad = [18,,14,15,92,78,63];
+console .log(edad[2]);
+let nombre = ["juliana","marvel","zuluaga","velez","santo"];
+console.log[nombre];
+
+let correo =[
+    "juliana@misena.edu.co",
+    "marvel@misena.edu.co",
+    "zuluaga@misena.edu.co",
+    "velez@misena.edu.co",
+    "santo@misena.edu.co"
+];
+console.log(correo.length); 
---
+++ tp9/function.js
@@ -0,0 +1,41 @@
+
+dato = [nombre,apellido,edad,telefono,direccion];
+console .log(edad[2]);
+let nombre= ["juliana","matias","carlos","simon","teresa"];
+console.log[nombre];
+let apellido =[
+    "martinez","correa","zapata","bedolla", "londoño"
+];
+console.log(apellido.length); 
+
+let telefono =[
+    "315427582", "276192", "7210720", "316508723", "29187265"
+];
+console.log(telefono.length); 
+
+let edad =[
+    "12", "15", "16", "18", "17",
+]
+let direccion =[
+    "diagonal-65","diagonal-60", "diagonal-45","diagonal-61", "diagonal-70"
+];
+console.log(direccion.length); 
+
+nombre.push("juliana");
+nombre.splice(juliana);
+
+/*for (let i=0; i<nombre.lenght;i++){
+    console.log (nombre[1] + "" + apellido[1] + "" + telefono + "" + edad[1] + "" + direccion[1]);
+    console.log(`${nombre[1]} ${apellido[1]} ${telefono[1]} ${adad[1]} ${direccion[1]}`);
+}
+*/
+/*
+nombre.forEach(Element -> {
+    console.log(`el nombre es  ${Element}`);
+
+});*/
+let persona ={"nombre":"luis","apellido":"vasquez","gustos":[futbol, basquet, natacion]};
+let agrega = {"edad":5};
+let curso =[{},{},{},{}]
+
+
