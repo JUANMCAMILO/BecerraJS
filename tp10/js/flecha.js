4b255bf75e576cbdda2a6bc19fee1bb7d6d5face
Kravmaga01 <edwardsalex8034@gmil.com>
2022-06-13 17:52:35 -0500

    sexto commit

---
+++ tp10/js/flecha.js
@@ -0,0 +1,15 @@
+let contador = document.getElementById("caja");
+ let tota=0;
+setInterval((n=30)=>{
+    tota +=1;
+    console.log(n)
+    contador.innerHTML= tota;
+},100)
+
+let cuadro = document.getElementById("caja");{
+    let contador=cuadro=>{
+        cuadro +=contador;
+        console.log(cuadro)
+    },cuadro
+    
+}
